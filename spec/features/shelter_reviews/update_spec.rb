5ee04f9b12fa6dfb47d60d55f977b783fdb30029
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-04 14:57:37 -0700

    Create update spec for shelter review

---
+++ spec/features/shelter_reviews/update_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "on shelter show page" do
+  before :each do
+    @shelter_1 = Shelter.create(name: "Blue Blue Barky",
+      address: "123 This Way",
+      city: "Denver",
+      state: "CO",
+      zip: "90204")
+
+    @review_1 = @shelter_1.shelter_reviews.create(title: "Good spot", rating: 4, content: "Lovely place down the street with cute dogs")
+    @review_2 = @shelter_1.shelter_reviews.create(title: "THE BEST", rating: 5, content: "THE BEST woooooo")
+  end
+
+  it "there is a link next to each review to edit that review" do
+
+
+    visit "/shelters/#{@shelter_1.id}"
+
+    within "#review-#{@review_1.id}" do
+      expect(page).to have_link("Edit")
+    end
+
+    within "#review-#{@review_2.id}" do
+      expect(page).to have_link("Edit")
+    end
+  end
+
+  it "when edit button is clicked user is taken to form with fields populated with current info" do
+
+    visit "/shelters/#{@shelter_1.id}"
+
+    within "#review-#{@review_1.id}" do
+      click_on "Edit"
+      expect(find_field(:title).value).to eq("Good spot")
+      expect(find_field(:rating).value).to eq(4)
+      expect(find_field(:content).value).to eq("Lovely place down the street with cute dogs")
+    end
+  end
+
+
+
+
+end
