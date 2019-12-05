e25f7154f0171d62befb3f3ebed66e4e1c7ecb8c
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-05 13:23:19 -0700

    Add check to see if favorites indicator decreases by 1 when pet is removed from favorites

--- spec/features/favorites/add_favorite_spec.rb
+++ spec/features/favorites/add_favorite_spec.rb
@@ -34,9 +34,12 @@ RSpec.describe "when user favorites a pet", type: :feature do
 
     click_on "Add To Favorites"
 
+    expect(page).to have_content("Favorites: 1")
+
     click_on "Remove From Favorites"
 
-    expect(page).to have_content("You have removed #{@pet_1.id} from your favorites.")
+    expect(page).to have_content("You have removed #{@pet_1.name} from your favorites.")
+    expect(page).to have_content("Favorites: 0")
     expect(page).to have_link("Add To Favorites")
   end
 
