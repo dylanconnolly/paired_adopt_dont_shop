08d8e0d7d0b7c989a1e375ee723dab458292854e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-05 12:23:31 -0700

    Add test for favorite counter linking to favorites page

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -28,4 +28,33 @@ RSpec.describe "favorite pets index page", type: :feature do
     expect(page).to_not have_link(pet_3.name)
     expect(page).to_not have_css("img[src='#{pet_3.image}']")
   end
+
+  it "user can click on favorite pets counter and be taken to favorites page" do
+    shelter_1 = Shelter.create(name: "Blue Blue Barky",
+      address: "123 This Way",
+      city: "Denver",
+      state: "CO",
+      zip: "90204")
+
+
+    pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
+    pet_2 = Pet.create(name: "Peter", image: "https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg", approximate_age: "1", sex: "male", shelter: shelter_1)
+
+    visit "/pets/#{pet_1.id}"
+    click_on "Add To Favorites"
+
+    visit "/shelters"
+    click_on "Favorites: 1"
+
+    expect(current_path).to eq("/favorites")
+    expect(page).to have_link(pet_1.name)
+
+    visit "/pets/#{pet_2.id}"
+    click_on "Add To Favorites"
+
+    visit "/"
+    click_on "Favorites: 2"
+
+    expect(current_path).to eq("/favorites")
+  end
 end
