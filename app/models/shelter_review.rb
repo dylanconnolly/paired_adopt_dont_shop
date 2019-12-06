2a3c925de358ac9a9249d6127972f9d4e4cf1523
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-05 23:29:34 -0700

    Remove pseudo code comment

--- app/models/shelter_review.rb
+++ app/models/shelter_review.rb
@@ -1,6 +1,5 @@
 class ShelterReview < ApplicationRecord
   validates_presence_of :title, :rating, :content
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
-# validates numericality of or inclusion of
   belongs_to :shelter
 end
