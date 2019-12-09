c4b01dbc341af6f10953eaa13192f0df48a1da47
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 15:22:53 -0700

    Create pet_applications controller

---
+++ app/controllers/pet_applications_controller.rb
@@ -0,0 +1,10 @@
+class PetApplicationsController < ApplicationController
+
+  def update
+    @pet_application =  PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:app_id])
+    @pet_application.toggle(:approved).save
+    @pet_application.pet.switch_adoption_status
+    
+    redirect_to "/pets/#{@pet_application.pet.id}"
+  end
+end
