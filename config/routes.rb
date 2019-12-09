2ae9bef712b5b0a5bc8f027a80dab58b94f17740
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 12:22:10 -0700

    Change route of applying for pets from favorites page to be applications/new

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   get '/favorites', to: 'favorites#index'
   delete '/favorites/:pet_id', to: 'favorites#destroy'
   delete '/favorites', to: 'favorites#destroy'
-  get '/favorites/apply', to: 'applications#new'
+  get '/applications/new', to: 'applications#new'
   post '/applications', to: 'applications#create'
   get '/applications/:app_id', to: 'applications#show'
 end
