1bcbdd83defc64d7fd02c4df1f4ea3bf4195bba1
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 11:53:41 -0700

    Add test to index spec for display message when there are no applications for pet

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "applications index" do
     @pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
     @pet_2 = Pet.create(name: "Peter", image: "https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg", approximate_age: "1", sex: "male", shelter: shelter_1)
     @pet_3 = Pet.create(name: "Matt", image: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
+    @pet_4 = Pet.create(name: "Doge", image: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
 
     @application_1 = Application.create!(name: "Dylan", address: "123 Main", city: "Denver", state: "CO", zip: "80203", phone: "555555", reason: "I am good owner")
     @application_2 = Application.create!(name: "Steve", address: "1273 Main", city: "Chicago", state: "IL", zip: "23832", phone: "12312314", reason: "I am bad owner")
@@ -34,5 +35,14 @@ RSpec.describe "applications index" do
     end
 
     expect(current_path).to eq("/applications/#{@application_1.id}")
+  end
+
+  it "if there are no applications for pet, user sees message stating such" do
+
+    visit "pets/#{@pet_4.id}"
+
+    within "#applications" do
+      expect(page).to have_content("There are no applications to adopt this pet.")
+    end
   end
 end
