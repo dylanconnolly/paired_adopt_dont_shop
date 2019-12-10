3440c8a256f3d203c4358b61cf887ffe82678089
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 21:53:08 -0700

    Create #find method for pet_application model:

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -6,4 +6,8 @@ class PetApplication < ApplicationRecord
     approved_statuses = select('approved').where("pet_id = #{pet.id}").pluck(:approved)
     approved_statuses.include?(true)
   end
+
+  def self.find(pet_id, app_id)
+    find_by(pet_id: pet_id, application_id: app_id)
+  end
 end
