35c4c7d012f674dfd07dab453aae285cb7d134ff
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-10 11:41:19 -0700

    Add pets_with_applications_approved method to shelter model

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,10 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
   has_many :shelter_reviews
+
+  def pets_with_approved_applications
+    self.pets.find_all do |pet|
+      PetApplication.pet_adopted?(pet) == true
+    end
+  end
 end
