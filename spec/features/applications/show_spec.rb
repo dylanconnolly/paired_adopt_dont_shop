af2bdf51e1b04a6e79674c4690e82a063707414a
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 21:26:58 -0700

    Add test to application show spec to test that Approve Adoption option will be removed if a pet is already adopted

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,9 +12,12 @@ RSpec.describe "application show page" do
     @pet_3 = Pet.create(name: "Matt", image: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
 
     @application = Application.create!(name: "Dylan", address: "123 Main", city: "Denver", state: "CO", zip: "80203", phone: "555555", reason: "I am good owner")
+    @application_2 = Application.create!(name: "Bob", address: "1123123 Main", city: "Pawnee", state: "IN", zip: "28383", phone: "123555555", reason: "I am person")
 
     @application.pets << @pet_1
     @application.pets << @pet_2
+    @application_2.pets << @pet_1
+    @application_2.pets << @pet_2
   end
 
   it "lists details of application and link to each pet on the application" do
@@ -60,6 +63,7 @@ RSpec.describe "application show page" do
   end
 
   it "allows user to be approved for any number of pets on the application" do
+
     visit "/applications/#{@application.id}"
 
     within "#pet-#{@pet_1.id}" do
@@ -78,5 +82,30 @@ RSpec.describe "application show page" do
     expect(current_path).to eq("/pets/#{@pet_2.id}")
     expect(page).to have_content("Adoptable: false")
     expect(page).to have_content("(On hold for #{@application.name}.)")
+  end
+
+  it "hides ability to approve application for a pet if that pet has already been adopted" do
+
+    visit "/applications/#{@application.id}"
+
+    within "#pet-#{@pet_1.id}" do
+      click_link "Approve Adoption"
+    end
+
+    visit "/applications/#{@application_2.id}"
+
+    within "#pet-#{@pet_1.id}" do
+      expect(page).to_not have_link("Approve Adoption")
+    end
+
+    within "#pet-#{@pet_2.id}" do
+      click_link "Approve Adoption"
+    end
+
+    visit "/applications/#{@application.id}"
+
+    within "#pet-#{@pet_1.id}" do
+      expect(page).to_not have_link("Approve Adoption")
+    end
   end
 end
