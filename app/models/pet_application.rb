7a246fe5ef148016e4751cd9195ad6c1b331d4d8
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-09 21:13:33 -0700

    Create pet_adopted? method for pet_application collection

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,9 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def self.pet_adopted?(pet)
+    approved_statuses = select('approved').where("pet_id = #{pet.id}").pluck(:approved)
+    approved_statuses.include?(true)
+  end
 end
