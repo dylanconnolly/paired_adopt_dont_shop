aca46deb1995d5363dfccea29e9308b1148982a7
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-11 13:09:14 -0700

    Add test to pet new_spec to check for flash messages upon success/failure creation

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -37,4 +37,27 @@ RSpec.describe "new pet view" do
     expect(page).to have_content("Catalina")
     find("img[src='https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg']")
   end
+
+  it "displays flash message upon failed/successful creation" do
+
+    visit "/shelters/#{@shelter_1.id}/pets/new"
+
+    fill_in "image", with: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg"
+    fill_in "name", with: ""
+    fill_in "description", with: "lovely little pupper"
+    fill_in "approximate_age", with: "6"
+    select "female", :from => :sex
+
+    click_button "Add Pet"
+
+    expect(page).to have_content("Pet creation failed: please complete all fields on the form.")
+
+    fill_in "image", with: "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_1280.jpg"
+    fill_in "name", with: "Catalina"
+    fill_in "description", with: "lovely little pupper"
+    fill_in "approximate_age", with: "6"
+    select "female", :from => :sex
+
+    expect(page).to have_content("Pet created successfully!")
+  end
 end
